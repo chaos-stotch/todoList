e3f8f2220dbc0c9df6aba46216fdbd42d92e0aa2
Vitnin <chaos_stotch@proton.me>
2022-11-13 01:53:12 -0300

    adding remaining routers

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,9 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-const tasksController = require("./controllers/tasksController")
+const tasksController = require("./controllers/tasksController");
+const tasksMiddler = require("./middlewares/tasksMiddlewares");
 
-router.get('/tasks', tasksController.getAll)
-router.post('/tasks', tasksController.createTask)
+router.get('/tasks', tasksController.getAll);
+router.post('/tasks', tasksMiddler.validateFieldTitle, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put('/tasks/:id',
+    tasksMiddler.validateFieldTitle,
+    tasksMiddler.validateFieldStatus,
+    tasksController.updateTask);
 
 module.exports = router;
